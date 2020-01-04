00e1add01f8cf13e060f016caaf70b1716722c80
andres <anieves929@gmail.com>
2020-01-03 23:42:11 -0500

    Problem 3

---
+++ Problem3.cpp
@@ -0,0 +1,38 @@
+/*Problem:
+ * Given an array of N unique integers and a target number t, return a list of triplets (a,b,c) such that a+b+c=t.*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+  string s;
+  int arr[argc-1];
+  cout<<"Input: [";
+  for(int i=1;i<argc;i++){
+    s=argv[i];
+    arr[i-1]=stoi(s);
+    cout<<arr[i-1];
+    if(i<argc-1)
+      cout<<",";
+  }
+  cout<<"]"<<"\n";
+  int t;
+  cout<<"Enter a target number: ";
+  cin>>t;
+/////////://////My Solution///////////////////
+  cout<<"Output: [";
+  for(int i=0;i<argc-1;i++){
+    for(int j=i+1;j<argc-1;j++){
+      for(int k=j+1;k<argc-1;k++){
+        //cout<<arr[i]<<" "<<arr[j]<<" "<<arr[k];
+        //cout<<"\n";
+        if(((arr[i]+arr[j])+arr[k])==t){
+          cout<<"("<<arr[i]<<","<<arr[j]<<","<<arr[k]<<")";
+        }
+      }
+    }
+  }
+  cout<<"]"<<"\n";
+  return 0;
+}
